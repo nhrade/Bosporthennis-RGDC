45c37f21b62f0ddc8e5b2a3faaee98e54d5cfe35
Noah Hradek <noahh717@gmail.com>
2015-02-15 19:04:57 -0500

    Added project structure and files

---
+++ Assets/Scripts/ProceduralGeneration.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProceduralGeneration : MonoBehaviour {
+    GameObject player;
+    private const float CHANCE = 0.2f;
+
+
+    bool PlayerMoving()
+    {
+        return player.rigidbody2D.velocity.magnitude > 0;
+    }
+	
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+
+    private void InstantiatePlanet(Vector2 dir)
+    {
+        GameObject planet = GameObject.Find("Planet");
+        Instantiate(planet, new Vector3((player.transform.position.x + camera.orthographicSize) * dir.x,
+            (player.transform.position.y + camera.orthographicSize) * dir.y), Quaternion.identity);
+        
+
+    }
+	
+	void Update () {
+        if (PlayerMoving())
+        {
+            Vector2 direction = player.rigidbody2D.velocity.normalized;
+            InstantiatePlanet(direction);
+        }
+	}
+}
